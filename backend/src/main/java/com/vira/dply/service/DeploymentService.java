f6fcf34ddac7e37d0b1a80dafb7d743ea9cf7afc
Víctor Gómez <39098525+victorgomez09@users.noreply.github.com>
2025-12-05 12:37:22 +0000

    remove angular frontend

--- backend/src/main/java/com/vira/dply/service/DeploymentService.java
+++ backend/src/main/java/com/vira/dply/service/DeploymentService.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 import org.springframework.stereotype.Service;
--- frontend/src/environments/environment.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-    API_URL: "https://verbose-fiesta-g5wg45vqjpphv5rw-8080.app.github.dev"
-};
